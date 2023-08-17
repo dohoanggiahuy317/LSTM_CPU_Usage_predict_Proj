b04be362520233c36bc016db81fdeab3e18ebf3f
dohoanggiahuy317 <dohoanggiahuy310703@gmail.com>
2023-08-17 00:29:04 -0400

    test api

---
+++ command/test.sh
@@ -0,0 +1,6 @@
+python code/test.py \
+    --eval_path "data/preprocess_data/test.csv" \
+    --model_path "model/cpu/H" \
+    --prev_day 10 \
+    --pred_day 3 \
+    --save_pred_path "Result/cpu/H/result.csv"
